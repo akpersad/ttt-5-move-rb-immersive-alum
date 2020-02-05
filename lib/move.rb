f3b98dd47e90de4efb7ba65e271c6eb6d4106d47
Andrew Persad <akpersad@gmail.com>
2020-02-05 02:03:06 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,6 +8,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def is_i?
+  !!(self =~ /\A[-+]?[0-9]+\z/)
+end
+
 # code your input_to_index and move method here!
 def input_to_index(arg)
   arg = arg.to_i
@@ -17,6 +21,8 @@ def input_to_index(arg)
     arg
   end
 end
+
+
 
 def move (board, position, choice = "X")
   board[input_to_index(position)] = choice
